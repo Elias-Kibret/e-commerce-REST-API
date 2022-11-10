76cc94a525593a480d76b72495e35458ac4453cd
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-10 14:57:01 +0300

    order

--- Controllers/order.js
+++ Controllers/order.js
@@ -1,4 +1,5 @@
 const orderCart = require("../Model/order");
+const router = require("../Routes/user");
 
 const getOrders = async (req, res) => {
 	try {
@@ -9,6 +10,61 @@ const getOrders = async (req, res) => {
 	}
 };
 
+const getSingleOrder = async (req, res) => {
+	try {
+		const singleOrder = await orderCart.findById({ userId: req.params.userId });
+		res.status(200).json(singleOrder);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
+const updateOrder = async (req, res) => {
+	try {
+		const updated = await orderCart.findOneAndUpdate(
+			{
+				userId: req.params.userId,
+			},
+			{
+				$set: req.body,
+			},
+			{
+				new: true,
+			}
+		);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
+//GET MONTHLY INCOME
+
+const getStats = async (req, res) => {
+	const date = new Date();
+	const lastmonth = new Date(date.setMonth(date.getMonth() - 1));
+	const previousMonth = new Date(new Date().setMonth(lastMonth() - 1));
+	try {
+		const income = await orderCart.aggregate([
+			{ $match: { createdAt: { $gte: previousMonth } } },
+			{
+				$project: {
+					month: { $month: "$createdAt" },
+					sales: "$amount",
+				},
+				$group: {
+					_id: "$month",
+					total: { $sum: "$sales" },
+				},
+			},
+		]);
+		res.status(200).json(income);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
 module.exports = {
 	getOrders,
+	getSingleOrder,
+	getStats,
 };
--- Routes/order.js
+++ Routes/order.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const { getOrders, getSingleOrder, getStats } = require("../Controllers/order");
+const { verfityTokenAndAdmin } = require("../Middleware/verfiyToken");
 
+router.get("/", verfityTokenAndAdmin, getOrders);
+router.get("/:userId", verfityTokenAndAdmin, getSingleOrder);
+router.get("/stats", verfityTokenAndAdmin, getStats);
 module.exports = router;
