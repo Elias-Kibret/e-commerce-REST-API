9bce3336e95d90d3bf32ba3c5e38924cab52c0ea
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-10 09:34:55 +0300

    create cart controller

--- Controllers/cart.js
+++ Controllers/cart.js
@@ -2,6 +2,17 @@ const cartModel = require("../Model/cart");
 const { findByIdAndDelete } = require("../Model/product");
 const router = require("../Routes/user");
 
+//create cart
+
+const createCart = async (req, res) => {
+	try {
+		const newCart = await new cartModel(req.body).save();
+		res.status(200).json(newCart);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
 const deleteCart = async (req, res) => {
 	try {
 		await findByIdAndDelete(req.params.id);
